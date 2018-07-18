f0a714b331366a2dd0e20942340435f65ba849ce
romanruz <finalreleaser@gmail.com>
2018-07-18 15:16:44 +0300

    animate

---
+++ js/animateWow.js
@@ -0,0 +1,31 @@
+window.addEventListener('scroll', function(e) {
+        if( $(window).scrollTop() <= 50) {
+            $('.wow').removeClass('animated');
+            $('.wow').removeAttr('style');
+            new WOW().init();
+        }
+});
+
+
+// wow slideInLeft" data-wow-iteration="1" data-wow-offset="10" data-wow-duration="2s" data-wow-delay="0.3s
+let h1dataOffset = 100;
+let h1 = document.getElementsByTagName('h1')
+
+
+for (i = 0; i < h1.length; i++) {
+    h1[i].classList.add('wow','slideInLeft');
+	h1[i].setAttribute ('data-wow-offset' ,h1dataOffset);
+}
+let h2dataOffset = 50;
+
+let h2 = document.getElementsByTagName('h2')
+
+for (i = 0; i < h2.length; i++) {
+    h2[i].classList.add('wow','rollIn');
+	h2[i].setAttribute ('data-wow-offset' ,h2dataOffset);
+	h2[i].setAttribute ('data-wow-delay' ,0);
+}
+
+
+
+new WOW().init();
