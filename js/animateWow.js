f07e0c1331b299d25affff84687e379260a61f79
romanruz <finalreleaser@gmail.com>
2018-07-19 09:22:23 +0300

    animate

--- js/animateWow.js
+++ js/animateWow.js
@@ -1,10 +1,11 @@
 window.addEventListener('scroll', function(e) {
-        if( $(window).scrollTop() <= 1 || $(window).scrollTop() == $(document).height() - $(window).height()-1) {
-        	console.log ('in');
-            $('.wow').removeClass('animated');
-            $('.wow').removeAttr('style');
-            new WOW().init();
-        }
+    if ($(window).scrollTop() <= 1 || $(window).scrollTop() == $(document).height() - $(window).height() - 1) {
+        console.log('in');
+        $('.wow').removeClass('animated');
+        $('.wow').removeAttr('style');
+        e.preventDefault();
+        new WOW().init();
+    }
 });
 
 
@@ -14,17 +15,17 @@ let h1 = document.getElementsByTagName('h1')
 
 
 for (i = 0; i < h1.length; i++) {
-    h1[i].classList.add('wow','slideInLeft');
-	h1[i].setAttribute ('data-wow-offset' ,h1dataOffset);
+    h1[i].classList.add('wow', 'slideInLeft');
+    h1[i].setAttribute('data-wow-offset', h1dataOffset);
 }
 let h2dataOffset = 50;
 
 let h2 = document.getElementsByTagName('h2')
 
 for (i = 0; i < h2.length; i++) {
-    h2[i].classList.add('wow','rollIn');
-	h2[i].setAttribute ('data-wow-offset' ,h2dataOffset);
-	h2[i].setAttribute ('data-wow-delay' ,0);
+    h2[i].classList.add('wow', 'rollIn');
+    h2[i].setAttribute('data-wow-offset', h2dataOffset);
+    h2[i].setAttribute('data-wow-delay', 0);
 }
 
 
