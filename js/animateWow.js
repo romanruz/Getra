73659523ded7c0c543aefb09e0c7b75e9cc1ef75
romanruz <finalreleaser@gmail.com>
2018-07-19 16:35:33 +0300

    animation

--- js/animateWow.js
+++ js/animateWow.js
@@ -14,7 +14,7 @@ let h1dataOffset = 100;
 let h1 = document.getElementsByTagName('h1')
 
 
-for (i = 0; i < h1.length; i++) {
+for (let i = 0; i < h1.length; i++) {
     h1[i].classList.add('wow', 'slideInLeft');
     h1[i].setAttribute('data-wow-offset', h1dataOffset);
 }
@@ -22,12 +22,19 @@ let h2dataOffset = 50;
 
 let h2 = document.getElementsByTagName('h2')
 
-for (i = 0; i < h2.length; i++) {
+for (let i = 0; i < h2.length; i++) {
     h2[i].classList.add('wow', 'rollIn');
     h2[i].setAttribute('data-wow-offset', h2dataOffset);
     h2[i].setAttribute('data-wow-delay', 0);
 }
 
+let picWrapper = document.getElementsByClassName ('picWrapper');
+let picWrapperA = picWrapper[0].getElementsByTagName('a');
+
+for(let i = 0;i<picWrapperA.length;i++){
+    console.log(picWrapperA[i])
+    picWrapperA[i].classList.add('wow', 'rollIn');
+}
 
 
 new WOW().init();
